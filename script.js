c0dea23a409b38e424660014020660fcc4792ea3
Kim Pham <km.phm2@gmail.com>
2025-07-30 22:48:07 +1000

    clean up, reset game when done

--- script.js
+++ script.js
@@ -1,80 +1,102 @@
-const randomWords = ['hello', 'goodbye', 'cat', 'dog'];
+const randomWords = ['dog'];
 
-const wordsToShow = randomWords.join(' ');
+function displayWords(arrayOfWords) {
+    const wordsToShow = arrayOfWords.join(' ');
 
-const charactersToShow = wordsToShow.split('');
-let currentCharacterIndex = 0;
+    const charactersToShow = wordsToShow.split('');
 
-charactersToShow.forEach((character) => {
-    const characterSpan = document.createElement('span');
-    characterSpan.className = 'letter';
-    characterSpan.innerText = character;
-    document.querySelector('.words').appendChild(characterSpan);
-});
+    const wordsContainer = document.querySelector('.words');
+    wordsContainer.innerHTML = '';
 
-document.querySelector('.letter').className = 'letter current';
+    charactersToShow.forEach((character) => {
+        const characterSpan = document.createElement('span');
+        characterSpan.className = 'letter';
+        characterSpan.innerText = character;
+        document.querySelector('.words').appendChild(characterSpan);
+    });
+}
 
-document.querySelector('.game').addEventListener('blur', (event) => {
-    console.log(event);
+document.querySelector('.game').addEventListener('blur', () => {
     document.querySelector('.offFocusMessage').style.opacity = '1';
     document.querySelector('.game').style.filter = 'blur(5px)';
 });
 
-document
-    .querySelector('.offFocusMessage')
-    .addEventListener('click', (event) => {
-        document.querySelector('.game').focus();
-    });
-
-document.querySelector('.game').addEventListener('focus', (event) => {
-    console.log(event);
+document.querySelector('.game').addEventListener('focus', () => {
     document.querySelector('.offFocusMessage').style.opacity = '0';
     document.querySelector('.game').style.filter = 'blur(0)';
 });
 
-document.querySelector('.game').addEventListener('keyup', (event) => {
-    console.log(event);
-    const keyPressed = event.key;
-    const key = event.key;
-    const currentWord = document.querySelector('.word.current');
-    const currentLetter = document.querySelector('.current');
+document.querySelector('.offFocusMessage').addEventListener('click', () => {
+    document.querySelector('.game').focus();
+});
 
-    const isBackspace = key === 'Backspace';
-
-    const cursor = document.querySelector('#cursor');
-    // const isFirstLetter = currentLetter === currentWord.firstChild;
-
-    console.log(key, currentLetter.textContent);
-
-    if (key == document.querySelector('.current').textContent) {
-        currentLetter.classList.add('correct');
-    } else {
-        if (key !== 'Backspace') {
-            currentLetter.classList.add('incorrect');
+function moveCursor(isBackspace, currentLetter) {
+    if (isBackspace && currentLetter.previousSibling) {
+        if (currentLetter.previousSibling.nodeType !== 3) {
+            cursor.style.left =
+                currentLetter.previousSibling.getBoundingClientRect().left +
+                'px';
         }
-    }
-
-    currentLetter.classList.remove('current');
-    if (isBackspace) {
-        cursor.style.left =
-            currentLetter.previousSibling.getBoundingClientRect().left + 'px';
-        currentLetter.previousSibling.classList.add('current');
-        currentLetter.previousSibling.classList.remove('correct');
-        currentLetter.previousSibling.classList.remove('incorrect');
     } else {
         cursor.style.left = currentLetter.nextSibling
             ? currentLetter.nextSibling.getBoundingClientRect().left + 'px'
             : currentLetter.getBoundingClientRect().right + 'px';
+    }
+}
+
+function moveCurrent(isBackspace, currentLetter) {
+    if (isBackspace && currentLetter.previousSibling) {
+        if (currentLetter.previousSibling.nodeType !== 3) {
+            currentLetter.previousSibling.classList.add('current');
+        }
+    } else {
         if (currentLetter.nextSibling) {
+            currentLetter.classList.remove('current');
             currentLetter.nextSibling.classList.add('current');
         }
     }
-    console.log('ko', document.querySelectorAll('.incorrect'));
+}
+
+function manageCorrectOrIncorrectStyle(key, isBackspace, currentLetter) {
+    if (isBackspace && currentLetter.previousSibling) {
+        if (currentLetter.previousSibling.nodeType !== 3) {
+            currentLetter.previousSibling.classList.remove('correct');
+            currentLetter.previousSibling.classList.remove('incorrect');
+        }
+    } else {
+        if (key === currentLetter.textContent) {
+            currentLetter.classList.add('correct');
+        } else {
+            currentLetter.classList.add('incorrect');
+        }
+    }
+}
+
+document.querySelector('.game').addEventListener('keyup', (event) => {
+    const key = event.key;
+    const isBackspace = key === 'Backspace';
+    const currentLetter = document.querySelector('.current');
+
+    moveCursor(isBackspace, currentLetter);
+    moveCurrent(isBackspace, currentLetter);
+
+    manageCorrectOrIncorrectStyle(key, isBackspace, currentLetter);
+
     if (!currentLetter.nextSibling) {
         if (document.querySelectorAll('.incorrect').length > 0) {
             alert('you have missing!');
         } else {
             alert('finish');
+            resetGame();
         }
     }
 });
+
+function resetGame() {
+    displayWords(randomWords);
+
+    document.querySelector('.letter').classList.add('current');
+    document.querySelector('#cursor').style.left = '20px';
+}
+
+resetGame();
